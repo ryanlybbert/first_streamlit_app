33f3a5252cb5b80880c9b5a1cd306308198bc83d
Ryan Lybbert <72362944+ryanlybbert@users.noreply.github.com>
2023-09-25 15:29:22 -0600

    Update streamlit_app.py
    Import fruityvice as json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -18,6 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display
 streamlit.dataframe(fruits_to_show)
 
-import requests
+#fruityvice api
+streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
