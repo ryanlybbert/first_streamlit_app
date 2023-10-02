1ffd0ac3aa3cecb95ed17b387fa8a27224f8702d
Ryan Lybbert <72362944+ryanlybbert@users.noreply.github.com>
2023-10-02 14:36:10 -0600

    from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(my_data_rows)
 #challenge
 add_my_fruit = streamlit.text_input("What fruit would you like to add?", '')
 streamlit.write('Thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values(", add_my_fruit, ")")
+my_cur.execute("insert into fruit_load_list values('from streamlit)")
 
 
 
